14984fe89a1c72a3bedf5579a493e87f9554967c
joomunludovic-arch <joomunludovic@me.com>
2025-07-26 13:51:27 -0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from analyseur import analyser_et_envoyer
+
+app = Flask(__name__)
+
+@app.route('/')
+def trigger():
+    try:
+        message = analyser_et_envoyer()
+        return f"✅ Script exécuté avec succès : {message}", 200
+    except Exception as e:
+        return f"❌ Erreur dans le script : {str(e)}", 500
+
+if __name__ == '__main__':
+    app.run()p
